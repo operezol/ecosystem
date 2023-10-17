61edcf4b12c4c82c66b176c491654b373a436171
operezol <oriolperezolivares@gmail.com>
2023-10-17 23:54:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
-let seeds = [];
+let entities = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     for (let i = 0; i < 10; i++) {
-        seeds.push(new Seed(random(width), random(height));
+        entities.push(new Entity(random(width), random(height)));
     }
 }
 
 function draw() {
     background(220);
-    for (let i = seeds.length - 1; i >= 0; i--) {
-        seeds[i].display();
-        seeds[i].spread();
+
+    for (let i = entities.length - 1; i >= 0; i--) {
+        entities[i].display();
+        if (!entities[i].isPlant && entities[i].readyToGrow()) {
+            entities[i].becomePlant();
+        }
+        entities[i].spread();
+        if (!entities[i].isPlant && !entities[i].isSeed) {
+            // Remove entities that are not plants and not seeds
+            entities.splice(i, 1);
+        }
     }
 }
 
-class Seed {
+class Entity {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.size = 10;
+        this.isPlant = false;
+        this.startTime = millis();
+        this.isSeed = true;
     }
 
     display() {
-        fill(255, 255, 0); // Yellow
+        if (this.isPlant) {
+            fill(0, 255, 0); // Green
+        } else {
+            fill(255, 255, 0); // Yellow
+        }
         square(this.x, this.y, this.size);
     }
 
+    readyToGrow() {
+        return !this.isPlant && millis() - this.startTime > 1000 && random(1) < 0.75 && !this.hasEntityAtPosition();
+    }
+
+    hasEntityAtPosition() {
+        for (let entity of entities) {
+            if (entity !== this && entity.isPlant && entity.x === this.x && entity.y === this.y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    becomePlant() {
+        this.isPlant = true;
+        this.size = 20; // Plant size
+        this.isSeed = false; // Mark it as not a seed
+    }
+
     spread() {
-        if (random(1) < 0.02) {
-            seeds.push(new Seed(this.x + round(random(-1, 1)) * this.size, this.y + round(random(-1, 1)) * this.size));
+        if (this.isPlant && random(1) < 0.02) {
+            let newX = this.x + round(random(-1, 1)) * this.size;
+            let newY = this.y + round(random(-1, 1)) * this.size;
+            if (!this.hasEntityAtPosition()) {
+                entities.push(new Entity(newX, newY));
+            }
         }
     }
 }
