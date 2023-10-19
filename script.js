f9b98bdba2abd871c0b48d3fc21f64d925e5e2da
Oriol <oriol@fxstreet.com>
2023-10-19 18:22:19 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let rainDrops = [];
-let fertilizer = [];
+let fertilizers = [];
 let plants = [];
 let wolves = [];
 let sheeps = [];
@@ -10,61 +10,55 @@ function setup() {
 
   for (let i = 0; i < 10; i++) {
     rainDrops.push(new RainDrop(random(width), random(height)));
-    fertilizer.push(new Fertilizer(random(width), random(height));
+  }
+  for (let i = 0; i < 10; i++) {
+    fertilizers.push(new Fertilizer(random(width), random(height), random(50)));
+  }
+  for (let i = 0; i < 10; i++) {
     seeds.push(new Seed(random(width), random(height)));
+  }
+  for (let i = 0; i < 10; i++) {
     plants.push(new Plant(random(width), random(height)));
-    wolves.push(new Wolf(/* TODO: define constructor params */));
-    sheeps.push(new Sheep(/* TODO: define constructor params */));
+  }
+  for (let i = 0; i < 10; i++) {
+    wolves.push(new Wolf(random(width), random(height)));
+  }
+  for (let i = 0; i < 10; i++) {
+    sheeps.push(new Sheep(random(width), random(height)));
   }
 }
 
 function draw() {
   background(220);
 
-  // Update and display raindrops
-  for (let i = rainDrops.length - 1; i >= 0; i--) {
-    let rainDrop = rainDrops[i];
-    if (rainDrop.isOffScreen()) {
-      rainDrops.splice(i, 1);
-    }else{
-        rainDrop.updateAndDisplay();
-    }
-  }
-  // Update and display fertilizer
-  for (let i = fertilizer.length - 1; i >= 0; i--) {
-    let f = fertilizer[i];
-    f.updateAndDisplay();
+  for (let i = 0; i < 10; i++) {
+    rainDrops.push(new RainDrop(random(width), random(height)));
   }
 
-// Update and display seeds
-for (let i = seeds.length - 1; i >= 0; i--) {
-    let s = seeds[i];
-    s.updateAndDisplay();
-    
-    // Determine if a seed becomes a plant
-    if (s.canBecomePlant()) {
-      // Replace the seed with a new plant
-      plants.push(new Plant(s.x, s.y));
-      seeds.splice(i, 1);
-    }
-  }
+  rainDrops.forEach(rainDrop => {
+    console.log(rainDrop);
+    rainDrop.updateAndDisplay();
+  });
 
-  // Update and display plants
-  for (let i = plants.length - 1; i >= 0; i--) {
-    let p = plants[i];
-    p.updateAndDisplay();
+  fertilizers.forEach(fertilizer => {
+    fertilizer.updateAndDisplay();
+  });
 
-    // Allow plants to absorb water and fertilizer
-    p.absorbWater();
-    p.absorbFertilizer();
+  seeds.forEach(seed => {
+    seed.updateAndDisplay();
+  });
 
-    // Determine if the plant has died
-    if (p.isDead()) {
-      // Replace the plant with fertilizer
-      fertilizer.push(new Fertilizer(p.x, p.y, p.mass));
-      plants.splice(i, 1);
-    }
-  }
+  plants.forEach(plant => {
+    plant.updateAndDisplay();
+  });
+
+  wolves.forEach(wolf => {
+    wolf.updateAndDisplay();
+  });
+
+  sheeps.forEach(sheep => {
+    sheep.updateAndDisplay();
+  });
 }
 
 class Seed {
@@ -72,18 +66,14 @@ class Seed {
     this.x = x;
     this.y = y;
     this.size = 10;
-    this.color = color(255, 255, 0); // Yellow color
+    this.color = color(255, 255, 0);
+    this.updateAndDisplay = this.updateAndDisplay.bind(this);
   }
 
   updateAndDisplay() {
-    // Display the seed
+    noStroke();
     fill(this.color);
     ellipse(this.x, this.y, this.size);
-  }
-
-  canBecomePlant() {
-    // Check if there is water and fertilizer nearby
-    // Implement the probability condition here
   }
 }
 
@@ -92,109 +82,80 @@ class Plant {
     this.x = x;
     this.y = y;
     this.size = 20;
-    this.color = color(0, 255, 0); // Green color
-    this.water = 1; // Initialize water supply
-    this.mass = 1; // Initialize mass
+    this.color = color(0, 255, 0);
+    this.updateAndDisplay = this.updateAndDisplay.bind(this);
   }
 
   updateAndDisplay() {
-    // Display the plant
+    noStroke();
     fill(this.color);
     ellipse(this.x, this.y, this.size);
-  }
-
-  absorbWater() {
-    // Implement water absorption from nearby raindrops
-    // Remove absorbed water drops from the canvas
-  }
-
-  absorbFertilizer() {
-    // Implement fertilizer absorption from nearby fertilizer
-    // Convert absorbed fertilizer into mass
-  }
-
-  isDead() {
-    // Check if the plant has died due to lack of water
   }
 }
 
 class Fertilizer {
-  // Fertilizer class definition here...
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color(100, 0, 0);
+    this.updateAndDisplay = this.updateAndDisplay.bind(this);
+  }
+
+  updateAndDisplay() {
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.size);
+  }
 }
 
 class RainDrop {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.size = 10;
-      this.color = color(0, 0, 255); // Blue color
-    }
-  
-    updateAndDisplay() {
-      // Move raindrop down the canvas
-      this.y += 5; // Adjust the speed as needed
-  
-      // Display the raindrop
-      fill(this.color);
-      rect(this.x, this.y, this.size, this.size);
-    }
-  
-    isOffScreen() {
-      return this.y > height;
-    }
-  }
-  class Fertilizer {
-    constructor(x, y, mass) {
-      this.x = x;
-      this.y = y;
-      this.size = 10;
-      this.color = color(139, 69, 19); // Brown color
-      this.mass = mass || 1; // Default mass is 1
-    }
-  
-    updateAndDisplay() {
-      // Display the fertilizer
-      fill(this.color);
-      rect(this.x, this.y, this.size, this.size);
-    }
-  }
-  
-class Plant {
-  constructor(/* TODO: Define constructor params */) {
-    // Initialize attributes
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 10;
+    this.color = color(0, 0, 255);
+    this.updateAndDisplay = this.updateAndDisplay.bind(this);
   }
 
   updateAndDisplay() {
-    // Implement the update and display logic for plants
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.size);
   }
 }
 
-class Wolf {
-  constructor(/* TODO: Define constructor params */) {
-    // Initialize attributes
-  }
-
-  updateAndDisplay() {
-    // Implement the update and display logic for wolves
-  }
-}
 
 class Sheep {
-  constructor(/* TODO: Define constructor params */) {
-    // Initialize attributes
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 10;
+    this.color = color(255, 255, 255);
+    this.updateAndDisplay = this.updateAndDisplay.bind(this);
   }
 
   updateAndDisplay() {
-    // Implement the update and display logic for sheeps
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.size);
   }
 }
 
-class Seed {
-  constructor(/* TODO: Define constructor params */) {
-    // Initialize attributes
+
+class Wolf {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 10;
+    this.color = color(255, 0, 0);
+    this.updateAndDisplay = this.updateAndDisplay.bind(this);
   }
 
   updateAndDisplay() {
-    // Implement the update and display logic for seeds
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.size);
   }
 }
+
